2ce5f12bd01c44a07c35ac910ad3b0bd908e85e5
Jason Reiss <jreiss@multitech.com>
2024-12-19 14:15:12 -0600

    thread_jit: decrease wait to 30ms, reduces tx failure when LBT is enabled.      Packets sent to radio less than 50ms before tx were being dropped

--- packet_forwarder/src/lora_pkt_fwd.c
+++ packet_forwarder/src/lora_pkt_fwd.c
@@ -3725,7 +3725,7 @@ void thread_jit(void) {
     int i;
 
     while (!exit_sig && !quit_sig) {
-        wait_ms(70);
+        wait_ms(30);
 
         for (i = 0; i < LGW_RF_CHAIN_NB; i++) {
             /* transfer data and metadata to the concentrator, and schedule TX */
